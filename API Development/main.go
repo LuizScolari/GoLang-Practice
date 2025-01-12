3ec5a88163e0e1e7aa801e9630bff6280d9182d7
Luiz Scolari <luizrobertoscolari@outlook.com>
2025-01-12 18:54:38 -0400

    Developing a todolist API

--- API Development/main.go
+++ API Development/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+type todoList struct {
+	ID          int    `json:"id"`
+	Task        string `json:"task"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
+
+var tasks = []todoList{
+	{ID: 0, Task: "Learn GoLang", Description: "Study and Practice GoLang", Completed: false},
+}
+
+func getTodoList(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, tasks)
+}
+
+func postTodoList(c *gin.Context) {
+	var newTask todoList
+
+	if err := c.BindJSON(&newTask); err != nil {
+		return
+	}
+
+	tasks = append(tasks, newTask)
+	c.IndentedJSON(http.StatusCreated, newTask)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/tasks", getTodoList)
+	router.POST("/tasks", postTodoList)
+
+	router.Run("localhost:5050")
 }
